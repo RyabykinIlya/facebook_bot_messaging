cfd9160e8bbaf6073e5bb77721e0cceebb10792c
RyabykinIlya <ryabykinilya@gmail.com>
2019-03-30 23:07:02 +0300

    hook

--- Bemeta/settings.py
+++ Bemeta/settings.py
@@ -28,7 +28,7 @@ F_API_VERSION = 'v3.2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
